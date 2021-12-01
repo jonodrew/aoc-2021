4b9ab04c2bc8580f16b63c12a5465b07f1f9a2f8
jonathan <jonodrew@users.noreply.github.com>
2021-12-01 16:00:22 +0000

    Clean up typing

--- day_01/source/__init__.py
+++ day_01/source/__init__.py
@@ -2,12 +2,12 @@ import itertools
 from typing import Generator
 
 
-def cast_data_to_int(datastream: Generator[int, None, None]):
+def cast_data_to_int(datastream: Generator[str, None, None]) -> Generator[int, None, None]:
     for datum in datastream:
         yield int(datum)
 
 
-def compare_measurements(measurements: Generator[int, None, None]):
+def compare_measurements(measurements: Generator[int, None, None]) -> int:
     first, second = itertools.tee(measurements, 2)
     comparisons = zip(first, itertools.islice(second, 1, None, None))
     return len([comparison for comparison in comparisons if comparison[1] > comparison[0]])
