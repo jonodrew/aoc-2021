82bc6b0129ae3b16d17f798a0ffc2a32a19684b4
jonathan <jonodrew@users.noreply.github.com>
2021-12-01 15:56:03 +0000

    Further reduce complexity
    This removes a lot of the state that was being held inside functions and replaces it with more iterators.

--- day_01/source/__init__.py
+++ day_01/source/__init__.py
@@ -8,19 +8,13 @@ def cast_data_to_int(datastream: Generator[int, None, None]):
 
 
 def compare_measurements(measurements: Generator[int, None, None]):
-    previous_depth = measurements.__next__()
-    larger_than_previous = 0
-    for measurement in measurements:
-        if measurement > previous_depth:
-            larger_than_previous += 1
-        previous_depth = measurement
-    return larger_than_previous
+    first, second = itertools.tee(measurements, 2)
+    comparisons = zip(first, itertools.islice(second, 1, None, None))
+    return len([comparison for comparison in comparisons if comparison[1] > comparison[0]])
 
 
 def generate_three_measurements(data_generator: Generator[int, None, None]) -> Generator[int, None, None]:
-    previous_window = iter((0, data_generator.__next__(), data_generator.__next__()))
-    for value in data_generator:
-        summable_window, current_window = itertools.tee(
-            itertools.chain(itertools.islice(previous_window, 1, 3), [value]), 2)
-        yield sum(summable_window)
-        previous_window = current_window
+    first, second, third = itertools.tee(data_generator, 3)
+    windows = zip(first, itertools.islice(second, 1, None, None), itertools.islice(third, 2, None, None))
+    for window in windows:
+        yield sum(window)
