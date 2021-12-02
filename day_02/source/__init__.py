ccf21c8c84bc57f406d1ffda3a30f07837910d97
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-02 13:59:32 +0000

    Simplify solve function

--- day_02/source/__init__.py
+++ day_02/source/__init__.py
@@ -1,6 +1,6 @@
 import itertools
 from functools import reduce
-from typing import Tuple, Generator
+from typing import Tuple, Generator, Callable
 import re
 import operator
 
@@ -12,7 +12,10 @@ def parse_word_and_number(line: str) -> Tuple[str, int]:
     :return:
     """
     re_match = re.match(r"(\w+) (\d+)", line)
-    return re_match.group(1), int(re_match.group(2))
+    if re_match is not None:
+        return re_match.group(1), int(re_match.group(2))
+    else:
+        raise ValueError
 
 
 def translate_instruction(instruction: Tuple[str, int]) -> Tuple[int, int]:
@@ -21,13 +24,17 @@ def translate_instruction(instruction: Tuple[str, int]) -> Tuple[int, int]:
         "up": lambda x, y: (0, int(y) * -1),
         "down": lambda x, y: (0, int(y)),
     }
-    return translation.get(instruction[0])(*instruction)
+    return translation.get(instruction[0], lambda x, y: (x, y))(*instruction)
 
 
 def calculate_position(
-    current_position: Tuple[int, int], new_position: Tuple[int, int]
-) -> Tuple[int, int]:
-    return current_position[0] + new_position[0], current_position[1] + new_position[1]
+    current_position: Tuple[int, int, int], new_instruction: Tuple[int, int]
+) -> Tuple[int, int, int]:
+    return (
+        current_position[0] + new_instruction[0],
+        current_position[1] + new_instruction[1],
+        0,
+    )
 
 
 def calculate_position_and_aim(
@@ -49,25 +56,26 @@ def calculate_position_and_aim(
 
 
 def solve_part_two(data_stream: Generator[str, None, None]) -> int:
-    return operator.mul(
-        *itertools.islice(
-            reduce(
-                calculate_position_and_aim,
-                map(translate_instruction, map(parse_word_and_number, data_stream)),
-                (0, 0, 0),
-            ),
-            2,
-        )
-    )
+    return solve(data_stream, calculate_position_and_aim, (0, 0, 0))
 
 
 def solve_part_one(data_stream: Generator[str, None, None]) -> int:
+    return solve(data_stream, calculate_position, (0, 0, 0))
+
+
+def solve(
+    data_stream: Generator[str, None, None],
+    position_function: Callable[
+        [Tuple[int, int, int], Tuple[int, int]], Tuple[int, int, int]
+    ],
+    initial_position: Tuple[int, int, int],
+):
     return operator.mul(
         *itertools.islice(
             reduce(
-                calculate_position,
+                position_function,
                 map(translate_instruction, map(parse_word_and_number, data_stream)),
-                (0, 0),
+                initial_position,
             ),
             2,
         ),
