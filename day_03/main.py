2c58fd7dddae8cf19a85f64932e9afafa3738ca8
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-03 16:31:04 +0000

    Tidy up main

--- day_03/main.py
+++ day_03/main.py
@@ -1,7 +1,5 @@
 import day_03.functions as functions
 from helpers import stream_data, current_path
 
-print(
-    f"Part one: {functions.solve_part_one(functions.gamma_rate(stream_data(current_path(__file__))), functions.epsilon_rate)}"
-)
+print(f"Part one: {functions.solve_part_one(stream_data(current_path(__file__)))}")
 print(f"Part two: {functions.solve_part_two(stream_data(current_path(__file__)))}")
