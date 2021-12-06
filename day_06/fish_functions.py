a6c7be3fb0c841ad926007cd569a3bf77cdcd909
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-06 15:13:29 +0000

    I've never been so mad...
    ...at a total refactor. This is the most painful lesson in over-optimising early I've had in a while

--- day_06/fish_functions.py
+++ day_06/fish_functions.py
@@ -1,45 +1,41 @@
-import itertools
-from dataclasses import dataclass
-from typing import Tuple, Union, Generator, Iterator
+from typing import Iterator
 
 
-@dataclass(frozen=True)
-class LanternFish:
-    time_to_spawn: int
+def population_on_day(day_to_find: int, population: Iterator[int]):
+    frequency_of_timers = map(tuple(population).count, (i for i in range(9)))
+    return recursive_frequency(frequency_of_timers, day_to_find)
 
 
-def spawn(default_period: int = 6, rest_time: int = 2) -> Tuple[LanternFish, LanternFish]:
-    return LanternFish(default_period), LanternFish(default_period + rest_time)
-
-
-def age(fish: LanternFish) -> Tuple[LanternFish]:
-    return LanternFish(fish.time_to_spawn - 1),
-
-
-def process_single_fish(fish: LanternFish) -> Union[Tuple[LanternFish], Tuple[LanternFish, LanternFish]]:
-    if fish.time_to_spawn == 0:
-        return spawn()
-    else:
-        return age(fish)
-
-
-def process_generation(generation: Generator[LanternFish, None, int]) -> Generator[LanternFish, None, int]:
-    return fish_generator(itertools.chain(*map(process_single_fish, generation)))
-
-
-def fish_generator(all_fish: Iterator[LanternFish]) -> Generator[LanternFish, None, int]:
-    for fish in all_fish:
-        yield fish
-    return 1
-
-
-def population_on_day(day_to_find: int, population: Generator[LanternFish, None, int]):
+def recursive_frequency(old_frequency: Iterator[int], day_to_find: int) -> int:
+    """
+    This function recursively seeks the frequency at where the day == 0
+    :param old_frequency:
+    :param day_to_find:
+    :return:
+    """
     if day_to_find == 0:
-        return len(tuple(population))
+        return sum(old_frequency)
     else:
-        return population_on_day(day_to_find - 1, process_generation(population))
+        return recursive_frequency(move_along(old_frequency), day_to_find - 1)
 
 
-def generate_day_zero_fish(filepath: str) -> Iterator[LanternFish]:
+def move_along(frequencies: Iterator[int]) -> Iterator[int]:
+    """
+    This moves every frequency along one, except the frequency of sixes - which is moved down but has the frequency of
+    zeroes added to it. Finally, the function yields the frequency of zeroes - which is the new frequency of eights.
+    This is because wherever there's a zero, there's (essentially) a new 6 and a new 8.
+    :param frequencies:
+    :return:
+    """
+    zeroes = next(frequencies)
+    for i, frequency in enumerate(frequencies):
+        if i == 6:
+            yield zeroes + frequency
+        else:
+            yield frequency
+    yield zeroes
+
+
+def generate_day_zero_fish(filepath: str) -> Iterator[int]:
     with open(filepath, "r") as fish_file:
-        return map(LanternFish, (int(timer) for line in fish_file.read().splitlines() for timer in line.split(",")))
+        return (int(timer) for line in fish_file.read().splitlines() for timer in line.split(","))
--- tests/test_fish_functions.py
+++ tests/test_fish_functions.py
@@ -5,14 +5,25 @@ from day_06.fish_functions import *
 
 @pytest.fixture
 def day_zero():
-    return fish_generator((LanternFish(timer) for timer in (3, 4, 3, 1, 2)))
+    return iter((3, 4, 3, 1, 2))
 
 
 @pytest.mark.parametrize(
     ["day", "expected"],
     [
-        (0, 5), (18, 26), (80, 5934)
+        (0, 5), (18, 26), (80, 5934), (256, 26984457539)
     ]
 )
 def test_population_on_day(day_zero, day, expected):
     assert population_on_day(day, day_zero) == expected
+
+
+@pytest.mark.parametrize(
+    ["initial", "expected"],
+    [
+        ([0, 1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8, 0]),
+        ([1, 2, 3, 4, 5, 6, 7, 8, 9], [2, 3, 4, 5, 6, 7, 9, 9, 1])
+    ]
+)
+def test_move_along(initial, expected):
+    assert list(move_along(iter(initial))) == expected
