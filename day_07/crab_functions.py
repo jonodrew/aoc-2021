3a00c50c94c0a9a6fabd56bd7337bc60728760a4
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-07 13:58:30 +0000

    Part two optimised with thanks to m'colleague David
    This uses a recursive function to calculate the minimum fuel value. This works because, having sorted the initial positions, the total fuel function always creates a normally-distributed bell curve.

--- day_07/crab_functions.py
+++ day_07/crab_functions.py
@@ -5,24 +5,38 @@ from day_06.fish_functions import generate_day_zero_fish
 
 
 def generate_initial_positions() -> Iterator[int]:
-    return generate_day_zero_fish("/Users/jonathankerr/projects/aoc-2021/day_07/data.txt")
+    return iter(sorted(generate_day_zero_fish("/Users/jonathankerr/projects/aoc-2021/day_07/data.txt")))
 
 
-def absolute_distance(current_point: int, final_point: int) -> int:
+def absolute_distance(final_point: int, current_point: int) -> int:
     return abs(current_point - final_point)
 
 
-def exponential_distance(current_point: int, final_point: int) -> int:
-    distance = absolute_distance(current_point, final_point)
+def exponential_distance(final_point: int, current_point: int) -> int:
+    distance = absolute_distance(final_point, current_point)
     return int((distance * (distance + 1)) / 2)
 
 
-def calculate_total_fuel(initial_positions: Iterator[int], final_position: int, exponential: bool = False) -> int:
-    distance_functions = {False: absolute_distance, True: exponential_distance}
-    return sum(map(functools.partial(distance_functions[exponential], final_point=final_position), initial_positions))
+def calculate_total_fuel_for_point(distance_func: Callable[[int, int], int], final_position: int,
+                                   initial_positions: Iterator[int]) -> int:
+    return sum(map(functools.partial(distance_func, final_position), initial_positions))
 
 
-def calculate_minimum_fuel_slowly(initial_position_generator: Callable[[], Iterator[int]], exponential=False):
+def generate_all_fuel_totals(initial_position_generator: Callable[[], Iterator[int]], exponential=False):
     range_of_positions = range(min(initial_position_generator()), max(initial_position_generator()) + 1)
-    return min(calculate_total_fuel(initial_position_generator(), position, exponential) for position in range_of_positions)
+    distance_funcs = {True: exponential_distance, False: absolute_distance}
+    for total in (calculate_total_fuel_for_point(distance_funcs[exponential], position, initial_position_generator()) for position in range_of_positions):
+        yield total
 
+
+def calculate_minimum_fuel(initial_position_generator: Callable[[], Iterator[int]], exponential=False) -> int:
+    all_totals = generate_all_fuel_totals(initial_position_generator, exponential)
+    return minimum_of_curve(next(all_totals), all_totals)
+
+
+def minimum_of_curve(current_total: int, totals: Iterator[int]) -> int:
+    next_total = next(totals)
+    if next_total > current_total:
+        return current_total
+    else:
+        return minimum_of_curve(next_total, totals)
--- tests/test_crab_functions.py
+++ tests/test_crab_functions.py
@@ -1,25 +1,29 @@
 from typing import Iterator
 
 import pytest
-from day_07.crab_functions import calculate_total_fuel, calculate_minimum_fuel_slowly
+from day_07.crab_functions import calculate_total_fuel_for_point, calculate_minimum_fuel, minimum_of_curve, absolute_distance
 
 
 @pytest.fixture
 def test_data():
-    return iter([16, 1, 2, 0, 4, 2, 7, 1, 2, 14])
+    return test_data_generator()
 
 
 def test_data_generator() -> Iterator[int]:
-    return iter([16, 1, 2, 0, 4, 2, 7, 1, 2, 14])
+    return iter(sorted([16, 1, 2, 0, 4, 2, 7, 1, 2, 14]))
 
 
 def test_calculate_fuel(test_data):
-    assert calculate_total_fuel(test_data, 2) == 37
+    assert calculate_total_fuel_for_point(absolute_distance, 2, test_data) == 37
 
 
 def test_slow_calculation():
-    assert calculate_minimum_fuel_slowly(test_data_generator) == 37
+    assert calculate_minimum_fuel(test_data_generator) == 37
 
 
 def test_slow_calculation_exponential():
-    assert calculate_minimum_fuel_slowly(test_data_generator, True) == 168
+    assert calculate_minimum_fuel(test_data_generator, True) == 168
+
+
+def test_minimum_of_curve():
+    assert minimum_of_curve(3, iter((2, 1, 2, 3))) == 1
