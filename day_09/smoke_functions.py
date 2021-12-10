f24b0f7e99b13c0540e12d3ba2043d37e7780bbb
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-10 17:10:22 +0000

    Fixed get_neighbours!

--- day_09/smoke_functions.py
+++ day_09/smoke_functions.py
@@ -3,8 +3,9 @@ import itertools
 import operator
 from typing import Tuple, List, Iterator, FrozenSet, Callable
 
+import helpers
 
-@functools.lru_cache
+
 def grid() -> Iterator[Iterator[int]]:
     with open(
             "/Users/jonathankerr/projects/aoc-2021/day_09/data.txt"
@@ -12,33 +13,34 @@ def grid() -> Iterator[Iterator[int]]:
         return ((int(x) for x in line.strip()) for line in heights_grid_file.readlines())
 
 
-@functools.lru_cache
 def get_value(grid_func: Callable[[], Iterator[Iterator[int]]], coords: Tuple[int, int]) -> int:
     return next((value for y, row in enumerate(grid_func()) for x, value in enumerate(row) if (x, y) == coords))
 
 
-@functools.lru_cache
-def get_neighbours(coordinates: Tuple[int, int], grid_func) -> Iterator[int]:
-    filter_func = functools.partial(check_on_map, len(grid_func()), len(grid_func()[0]))
-    neighbour_coords = filter(filter_func, generate_neighbour_coords(coordinates))
-    return (get_value(grid_func, (x, y)) for x, y in neighbour_coords)
+def grid_height(grid_func: Callable[[], Iterator[Iterator[int]]]) -> int:
+    return helpers.iterator_length(grid_func())
 
 
-@functools.lru_cache
-def check_on_map(grid_height: int, grid_length: int, coords: Tuple[int, int]) -> bool:
-    return grid_length > coords[0] >= 0 and grid_height > coords[1] >= 0
+def grid_length(grid_func: Callable[[], Iterator[Iterator[int]]]) -> int:
+    return helpers.iterator_length(next(grid_func()))
 
 
-@functools.lru_cache
-def generate_neighbour_coords(coordinates: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
+def get_neighbour_values(coordinates: Tuple[int, int], grid_func: Callable[[], Iterator[Iterator[int]]]) -> Iterator[int]:
+    return (get_value(grid_func, (x, y)) for x, y in generate_neighbour_coords(coordinates, grid_func))
+
+
+def check_on_map(height: int, length: int, coords: Tuple[int, int]) -> bool:
+    return length > coords[0] >= 0 and height > coords[1] >= 0
+
+
+def generate_neighbour_coords(coordinates: Tuple[int, int], grid_func) -> Iterator[Tuple[int, int]]:
     x, y = coordinates
-    return iter([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
+    return filter(functools.partial(check_on_map, grid_height(grid_func), grid_length(grid_func)), iter([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]))
 
 
-@functools.lru_cache
 def get_non_nine_neighbours(grid_func, coord: Tuple[int, int]) -> List[Tuple[int, int]]:
     filter_func = functools.partial(check_on_map, len(grid_func()), len(grid_func()[0]))
-    neighbour_coords = filter(filter_func, generate_neighbour_coords(coord))
+    neighbour_coords = filter(filter_func, generate_neighbour_coords(coord, grid_func))
     return list(
         filter(
             lambda new_coord: grid_func()[new_coord[1]][new_coord[0]] != 9, neighbour_coords
@@ -47,19 +49,19 @@ def get_non_nine_neighbours(grid_func, coord: Tuple[int, int]) -> List[Tuple[int
 
 
 def is_local_minimum(grid_func, coord: Tuple[int, int]) -> bool:
-    neighbours = get_neighbours(coord, grid_func)
-    value = grid_func()[coord[1]][coord[0]]
+    neighbours = get_neighbour_values(coord, grid_func)
+    value = get_value(grid_func, coord)
     return all(map(lambda x: x > value, neighbours))
 
 
-def find_minima(grid_func: Callable) -> FrozenSet[Tuple[int, int]]:
-    all_coords = ((x, y) for x in range(len(grid_func()[0])) for y in range(len(grid_func())))
+def find_minima(grid_func: Callable[[], Iterator[Iterator[int]]]) -> FrozenSet[Tuple[int, int]]:
+    all_coords = itertools.product(range(grid_length(grid_func)), range(grid_height(grid_func)))
     skip_coords: FrozenSet = frozenset()
     local_minima: FrozenSet = frozenset()
     for coord in filter(lambda next_coord: next_coord not in skip_coords, all_coords):
         if is_local_minimum(grid_func, coord):
             local_minima = frozenset((*local_minima, coord))
-            skip_coords = frozenset((*skip_coords, *generate_neighbour_coords(coord)))
+            skip_coords = frozenset((*skip_coords, *generate_neighbour_coords(coord, grid_func)))
     return local_minima
 
 
--- tests/test_smoke_functions.py
+++ tests/test_smoke_functions.py
@@ -2,27 +2,27 @@ import unittest.mock
 
 import pytest
 
-from day_09.smoke_functions import get_neighbours, find_minima, find_basin_size, find_three_largest_and_get_product, \
-    find_all_basin_sizes
+from day_09.smoke_functions import get_neighbour_values, find_minima, find_basin_size, find_three_largest_and_get_product, \
+    find_all_basin_sizes, get_value
 
 
 def mock_grid():
     test_grid = ((2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
-                         (3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
-                         (9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
-                         (8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
-                         (9, 8, 9, 9, 9, 6, 5, 6, 7, 8))
+                 (3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
+                 (9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
+                 (8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
+                 (9, 8, 9, 9, 9, 6, 5, 6, 7, 8))
     return ((x for x in line) for line in test_grid)
 
 
 @pytest.mark.parametrize(
-        ["coord", "neighbours"],
-        [
-            ((0, 0), [1, 3]), ((9, 0), [1, 1]), ((0, 4), [8, 8]), ((9, 4), [7, 9]), ((4, 1), [7, 9, 9, 7])
-        ]
-    )
+    ["coord", "neighbours"],
+    [
+        ((0, 0), [1, 3]), ((9, 0), [1, 1]), ((0, 4), [8, 8]), ((9, 4), [7, 9]), ((4, 1), [7, 9, 9, 7])
+    ]
+)
 def test_get_neighbours(coord, neighbours):
-    assert list(get_neighbours(coord, mock_grid)) == neighbours
+    assert list(get_neighbour_values(coord, mock_grid)) == neighbours
 
 
 def test_find_minima():
@@ -30,11 +30,11 @@ def test_find_minima():
 
 
 @pytest.mark.parametrize(
-        ["minimum", "basin_size"],
-        (
-                ((1, 0), 3), ((9, 0), 9), ((2, 2), 14), ((6, 4), 9)
-        )
+    ["minimum", "basin_size"],
+    (
+            ((1, 0), 3), ((9, 0), 9), ((2, 2), 14), ((6, 4), 9)
     )
+)
 def test_find_basin_size(minimum, basin_size):
     assert find_basin_size(mock_grid, iter((minimum,))) == basin_size
 
@@ -47,5 +47,11 @@ def test_find_three_largest_and_get_product():
     assert find_three_largest_and_get_product(mock_grid) == 1134
 
 
-
-
+@pytest.mark.parametrize(
+    ("coords", "expected"),
+    [
+        ((9, 0), 0), ((0, 4), 9)
+    ]
+)
+def test_get_value(coords, expected):
+    assert get_value(mock_grid, coords) == expected
