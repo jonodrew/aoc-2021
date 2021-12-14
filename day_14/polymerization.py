87407311f0cb2cf4e4734212525e53d77e071f58
jonathankerr <jonathan.kerr@education.gov.uk>
2021-12-14 20:18:05 +0000

    Part two: total rewrite

--- day_14/polymerization.py
+++ day_14/polymerization.py
@@ -1,63 +1,84 @@
 import functools
-import itertools
-from typing import Iterator, Callable, Dict, Union, Tuple, List
-from helpers.immutable_dict import update_dict
+from typing import Iterator, Dict, Tuple, List
+
+from helpers.immutable_dict import update_dict, combine_dicts
 from more_itertools import sliding_window
 
 
-def feed_input(file_path: str = "./day_14/data.txt") -> Iterator[str]:
+def feed_input(file_path: str = "./day_14/data.txt") -> List[str]:
     with open(file_path) as points_file:
-        return filter(lambda line: line != '', map(lambda line: line.strip(), points_file.readlines()))
+        return list(filter(lambda line: line != '', map(lambda line: line.strip(), points_file.readlines())))
 
 
 def polymer_template() -> str:
-    return next(feed_input())
+    return feed_input()[0]
 
 
 def pair_insertion_rules() -> Iterator[str]:
-    yield from itertools.islice(feed_input(), 1, None)
+    yield from iter(feed_input()[1:])
 
 
-def convert_rule_to_map(rule_map: Dict[str, str], pair_insertion_rule: str) -> Dict[str, str]:
-    pair = pair_insertion_rule[0:2]
+def convert_rule_to_map(rule_map: Dict[Tuple[str, str], str], pair_insertion_rule: str) -> Dict[Tuple[str, str], str]:
+    pair = tuple(pair_insertion_rule[0:2])
     element = pair_insertion_rule[-1]
     return update_dict(rule_map, pair, element)
 
 
-@functools.lru_cache
-def map_of_all_rules() -> Dict[str, str]:
+def map_of_all_rules() -> Dict[Tuple[str, str], str]:
     return functools.reduce(convert_rule_to_map, pair_insertion_rules(), dict())
 
 
-@functools.lru_cache
 def get_new_element(window: Tuple[str, str]) -> str:
-    pair = ''.join(window)
-    return map_of_all_rules().get(pair)
+    return map_of_all_rules().get(window)
 
 
-def insert_new_elements(polymer: str, new_elements: Iterator[str]) -> str:
-    return "".join(old + new for old, new in itertools.zip_longest(polymer, new_elements, fillvalue=""))
-
-
-def polymer_pairs(polymer: str) -> Iterator[Tuple[str, str]]:
+def polymer_pairs(polymer: Iterator[str]) -> Iterator[Tuple[str, str]]:
     return sliding_window(polymer, 2)
 
 
-def process_polymer(polymer: str, steps: int = 1) -> str:
-    if steps == 0:
-        return polymer
+def new_dict_from_pair(old_pair_count: Tuple[Tuple[str, str], int]) -> Iterator[Dict[Tuple[str, str], int]]:
+    pair, count = old_pair_count
+    new_pairs = pair_from_rule(pair)
+    return combine_dicts(*map(lambda new_pair: {new_pair: count}, new_pairs))
+
+
+def convert_pair_map_to_char_count(char_map: Dict[Tuple[str, str], int], char_count: Dict[str, int]) -> Dict[str, int]:
+    if not char_map:
+        return add_trailing_char(char_count)
     else:
-        new_elements = map(get_new_element, polymer_pairs(polymer))
-        new_polymer = insert_new_elements(polymer, new_elements)
-        return process_polymer(new_polymer, steps - 1)
+        pair, count = char_map.popitem()
+        new_char_count = combine_dicts(char_count, {pair[0]: count})
+        return convert_pair_map_to_char_count(char_map, new_char_count)
 
 
-def count_elements(polymer: str) -> List[Tuple[str, int]]:
-    grouped_elems = itertools.groupby(sorted(polymer))
-    elems_with_counts = map(lambda key_group: (key_group[0], len(list(key_group[1]))), grouped_elems)
-    return sorted(elems_with_counts, reverse=True, key=lambda elem_tuple: elem_tuple[1])
+def add_trailing_char(char_map: Dict[str, int]) -> Dict[str, int]:
+    trailing_char = polymer_template()[-1]
+    return update_dict(char_map, trailing_char, 1)
 
 
-def subtract_least_common_element_from_most(polymer: str) -> int:
-    groups = count_elements(polymer)
-    return groups[0][1] - groups[-1][1]
+def calculate_char_map(char_map: Dict[Tuple[str, str], int], steps: int) -> Dict[Tuple[str, str], int]:
+    if steps == 0:
+        return char_map
+    else:
+        new_char_map = functools.reduce(combine_dicts, map(new_dict_from_pair, char_map.items()), dict())
+        return calculate_char_map(new_char_map, steps - 1)
+
+
+def pair_from_rule(pair: Tuple[str, str]) -> Tuple[Tuple[str, str], Tuple[str, str]]:
+    new_char = get_new_element(pair)
+    return (pair[0], new_char), (new_char, pair[1])
+
+
+def process_pair_map(steps: int) -> Dict[Tuple[str, str], int]:
+    initial_char_map = {window: 1 for window in polymer_pairs(polymer_template())}
+    return calculate_char_map(initial_char_map, steps)
+
+
+def count_elements_after_steps(steps: int):
+    processed_pair_map = process_pair_map(steps)
+    return convert_pair_map_to_char_count(processed_pair_map, dict())
+
+
+def subtract_least_common_element_from_most(steps: int) -> int:
+    groups = count_elements_after_steps(steps)
+    return max(groups.values()) - min(groups.values())
--- tests/test_polymerization.py
+++ tests/test_polymerization.py
@@ -1,16 +1,41 @@
+import functools
 from unittest.mock import patch
 
 import pytest
 
-from day_14.polymerization import process_polymer, feed_input
+from day_14.polymerization import feed_input, count_elements_after_steps, subtract_least_common_element_from_most
+
+partial_input = functools.partial(feed_input, "./data/test_data_14.txt")
 
 
 @pytest.mark.parametrize(
-    ("steps", "expected"),
-    [
-        (1, "NCNBCHB"), (2, "NBCCNBBBCBHCB")
+    "steps, expected", [
+        (1, {"N": 2, "B": 2, "C": 2, "H": 1})
     ]
 )
-def test_process_polymer(steps, expected):
-    with patch("day_14.polymerization.feed_input", return_value=feed_input("./data/test_data_14.txt")):
-        assert process_polymer("NNCB", steps) == expected
+@patch("day_14.polymerization.feed_input", return_value=partial_input())
+def test_count_elements_after_steps(mock_input, steps, expected):
+    assert count_elements_after_steps(steps) == expected
+
+
+@patch("day_14.polymerization.feed_input", return_value=partial_input())
+def test_count_elements(mock_input):
+    elements = count_elements_after_steps(40)
+    assert elements.get("H") == 3849876073
+    assert elements.get("B") == 2192039569602
+
+
+@pytest.mark.parametrize(
+    ["steps", "expected"],
+    [
+        (10, 1588), (40, 2188189693529)
+    ]
+)
+@patch("day_14.polymerization.feed_input", return_value=partial_input())
+def test_subtract_least_common_element_from_most(mock_input, steps, expected):
+    assert subtract_least_common_element_from_most(steps) == expected
+
+
+def test_real_count():
+    with patch("day_14.polymerization.feed_input", return_value=feed_input("/Users/jonathankerr/projects/aoc-2021/day_14/data.txt")):
+        assert subtract_least_common_element_from_most(10) == 2408
