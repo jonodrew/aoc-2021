762c5c4fe00ce890eda4aa3fd47ccff65571cb41
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-10-29 22:09:26 +0100

    At last, we're parsing correctly!

--- tests/tests_day_17/test_snailfish.py
+++ tests/tests_day_17/test_snailfish.py
@@ -7,8 +7,26 @@ from day_17.snailfish import Number, parse_number
     ["example", "number"],
     [
         ("[1,2]", Number("1", "2")),
-        ("[[1,2],3]", Number(Number("1", "2"), "3"))
-    ]
+        ("[[1,2],3]", Number(Number("1", "2"), "3")),
+        ("[9,[8,7]]", Number("9", Number("8", "7"))),
+        ("[[1,9],[8,5]]", Number(Number("1", "9"), Number("8", "5"))),
+        (
+            "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
+            Number(
+                left=Number(
+                    left=Number(
+                        left=Number(left="1", right="2"),
+                        right=Number(left="3", right="4"),
+                    ),
+                    right=Number(
+                        left=Number(left="5", right="6"),
+                        right=Number(left="7", right="8"),
+                    ),
+                ),
+                right="9",
+            ),
+        ),
+    ],
 )
 def test_parse_number(example, number):
     assert parse_number(example) == number
